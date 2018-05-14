517126fb4279216c09032646c5dbb7521daaa7d1
iSkyBell <hernan.pk92@hotmail.com>
2018-05-14 10:43:45 -0500

    Lista de animales
    Se agrega la lista de animales con su nombre y su imagen respectiva en una lista.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ANIMALES } from './../../data/data.animales';
+import { Animal } from './../../interfaces/animal.interface';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
 
+  constructor(public navCtrl: NavController) {
+    this.animales = ANIMALES.slice(0);
   }
 
 }
